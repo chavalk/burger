44f154d76a4b96a68e702a827a5b3315d6a7fe3c
chavalk <chavalk@hotmail.com>
2021-01-24 23:21:25 -0600

    Completed server.js to make it functional.

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-// Require express
+// Set up express
 var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Set handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them
+var routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+// Start server so it can begin listening to client requests
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
