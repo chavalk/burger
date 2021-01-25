fe47ba1bc0592657b9f5f1f0476ef703bd7bf311
chavalk <chavalk@hotmail.com>
2021-01-24 23:41:24 -0600

    Added route to get all items from table in burgers_controller and code for items to show in index.handlebars.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,5 +7,16 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions
 var burger = require("../models/burger");
 
+// Create routes and set up logic within those routes where needed
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
 // Export routes for server.js to use
 module.exports = router;
