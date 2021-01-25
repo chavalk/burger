20252f61ab82f908e3f42290dd45749fc5dcc0b2
chavalk <chavalk@hotmail.com>
2021-01-24 22:44:36 -0600

    Added models folder and burger.js with one method to interact with database.

---
+++ models/burger.js
@@ -0,0 +1,13 @@
+// Import the ORM to create functions that will interact with the database
+var orm = require("../config/orm");
+
+var burger = {
+    all: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (burgers_controller.js)
+module.exports = burger;
