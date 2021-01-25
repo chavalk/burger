7d6c6f61468d63f53a53977bdd1c08400722aef3
chavalk <chavalk@hotmail.com>
2021-01-24 21:33:34 -0600

    Modified comment in connection.js. Added orm.js with code to selectAll method for table queries.

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+// Import MySQL connection
+var connection = require("../config/connection");
+
+// Object for all SQL statement functions
+var orm = {
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+
+// Export the orm object for the model (burger.js)
+module.exports = orm;
