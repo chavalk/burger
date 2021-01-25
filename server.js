8385ca85f30ab9c3e394377b7a19df1b48f84e71
chavalk <chavalk@hotmail.com>
2021-01-25 00:22:42 -0600

    Added public, assets, and css folders. Added burger_style.css. Added access to public folder in server.js. Linked css stylesheet in main.handlebars.

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+// Serve static content for the app from the public directory in the application
+app.use(express.static("public"));
+
 // Set handlebars
 var exphbs = require("express-handlebars");
 
