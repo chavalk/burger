327801b85d184a18fee600ae25b84875525b7546
chavalk <chavalk@hotmail.com>
2021-01-26 11:58:29 -0600

    Added comments to CSS file and burgers.js.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,10 +1,16 @@
 $(function() {
+
+    // Listener for creating a new burger
     $(".create-form").on("submit", function(event) {
+        // Prevent default behavior
          event.preventDefault();
 
+        //  Create object to save name entered by user
          var newBurger = {
              burger_name: $("#burger").val().trim()
          };
+
+        // Ajax request to send new burger to route
          $.ajax("/api/burgers", {
              type: "POST",
              data: newBurger
@@ -14,13 +20,15 @@ $(function() {
          })
     });
 
+    // Listener for updating burgers
     $(".change-devour").on("click", function(event) {
+        // Grabbing id from burger and storing it in variable
         var id = $(this).data("id");
 
+        // Ajax request to send burger id to route
         $.ajax("/api/burgers/" + id, {
             type: "PUT"
         }).then(function() {
-            
             location.reload();
         });
     });
