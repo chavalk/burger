b5b63936bd539fd156a00a9cfc7cbbd03687228d
chavalk <chavalk@hotmail.com>
2021-01-26 12:48:16 -0600

    Added comment to burger.js.

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,7 @@
 // Import the ORM to create functions that will interact with the database
 var orm = require("../config/orm");
 
+// Burger functions
 var burger = {
     all: function(cb) {
         orm.selectAll("burgers", function(res) {
