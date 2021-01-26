ea0492a68b3dcf36ec31ed9271ce7631dc7b17dd
chavalk <chavalk@hotmail.com>
2021-01-26 12:08:06 -0600

    Added comments to burgers_controller.js.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,23 +9,29 @@ var burger = require("../models/burger");
 
 // Create routes and set up logic within those routes where needed
 router.get("/", function(req, res) {
+    // Call all method in burger.js to request all burgers
     burger.all(function(data) {
+        // Store burgers in object
         var hbsObject = {
             burgers: data
         };
-        // console.log(hbsObject);
+        // Render handlebars page and pass it object with burgers data
         res.render("index", hbsObject);
     });
 });
 
 router.post("/api/burgers", function(req, res) {
+    // Call create method in burger.js and pass it the burger name from the req object
     burger.create(req.body.burger_name, function(result) {
+        // Redirect browser to get route to load page with new burger included
         res.redirect("/");
     });
 });
 
 router.put("/api/burgers/:id", function(req, res) {
+    // Call update method in burger.js and pass it id in parameter
     burger.update(req.params.id, function(result) {
+        // If no rows are changed return 404 status else return 200 status
         if (result.changedRows == 0) {
             return res.status(404).end();
         } else {
